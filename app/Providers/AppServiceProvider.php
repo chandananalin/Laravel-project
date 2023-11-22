3204c2997b8d4e20f0b2975e521d96df0576cdd0
Chandana Nalin Cooray <chandanamine@gmail.com>
2023-11-22 13:26:24 +0530

    mody

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
+     * Register any application services.ee
      */
     public function register(): void
     {
